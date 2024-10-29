7da2054782cc0c92fbc0ead31bcf6437f72ff5c0
MincheolSin <smc970417@gmail.com>
2024-10-29 17:18:57 +0900

    first

---
+++ src/main/java/io/booksan/booksan_chat/BooksanChatApplication.java
@@ -0,0 +1,13 @@
+package io.booksan.booksan_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksanChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksanChatApplication.class, args);
+	}
+
+}
