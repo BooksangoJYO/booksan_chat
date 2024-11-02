98687762a165df6e8cd69e67225c9c06efca4319
MincheolSin <smc970417@gmail.com>
2024-11-02 09:39:09 +0900

    chat feature ver1

--- src/main/java/io/booksan/booksan_chat/dto/ChatMessageDTO.java
+++ src/main/java/io/booksan/booksan_chat/dto/ChatMessageDTO.java
@@ -1,16 +1,66 @@
 package io.booksan.booksan_chat.dto;
 
-import java.util.Date;
+import io.booksan.booksan_chat.entity.ChatRoom;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessageDTO {
-    private int messageId;
-    private String roomId;
-    private String uid;
-    private String content;
-    private Date insertDaytime;
+  // 메시지 타입 : 입장, 채팅, 나가기
+  public enum MessageType {
+      ENTER, TALK, LEAVE
+  }
+
+  private MessageType type; // 메시지 타입
+  private String roomId; // 방번호
+  private String sender; // 메시지 보낸사람
+  private String message; // 메시지
+  @Builder.Default
+  private int userCount = 0; //  채팅방에 있는 사용자수
+  
+  //입장 메시지 객체를 생성한다 
+  public static ChatMessageDTO enterMessage(ChatRoom chatRoom, String sender) {
+  	ChatMessageDTO result = ChatMessageDTO
+			  		.builder()
+			  		.type(MessageType.ENTER)
+			  		.roomId(chatRoom.getRoomId())
+			  		.sender(sender)
+			  		.userCount(chatRoom.getUserCount())
+			  		.build();
+  	result.typeProcess();
+  	return result;
+  }
+
+  //입장 메시지 객체를 생성한다 
+  public static ChatMessageDTO leaveMessage(ChatRoom chatRoom, String sender) {
+  	ChatMessageDTO result = ChatMessageDTO
+			  		.builder()
+			  		.type(MessageType.LEAVE)
+			  		.roomId(chatRoom.getRoomId())
+			  		.sender(sender)
+			  		.userCount(chatRoom.getUserCount())
+			  		.build();
+  	result.typeProcess();
+  	return result;
+  }
+  
+  public void typeProcess() {
+  	switch(type) {
+  	case ENTER:
+  		message = sender + "님이 입장하셨습니다.";
+  		sender = "[알림]";
+  		break;
+  	case LEAVE:
+  		message = sender + "님이 퇴장하셨습니다.";
+  		sender = "[알림]";
+  		break;
+  	default:
+  	}
+  	
+  }
 }
--- src/main/java/io/booksan/booksan_chat/service/ChatService.java
+++ src/main/java/io/booksan/booksan_chat/service/ChatService.java
@@ -2,31 +2,65 @@ package io.booksan.booksan_chat.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
-import io.booksan.booksan_chat.dao.ChatDAO;
-import io.booksan.booksan_chat.dto.ChatRoomDTO;
-import io.booksan.booksan_chat.util.MapperUtil;
+import io.booksan.booksan_chat.dto.AlarmMessageDTO;
+import io.booksan.booksan_chat.dto.ChatMessageDTO;
+import io.booksan.booksan_chat.entity.ChatRoom;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
-    private final ChatDAO chatDAO;
-    private final MapperUtil mapperUtil;
-
-    public List<ChatRoomDTO> findAllRoom(String uid) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //return chatDAO.findAllRoomById(uid).stream().map(room->mapperUtil.map(room,ChatRoomDTO.class)).toList();
-    }
-
-    public ChatRoomDTO createChatRoom(String name) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public ChatRoomDTO findRoomById(String roomId) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
+  private final ChatRoomService chatRoomService;
+ @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
     
+  
+	public List<ChatRoom> findAllRoom() {
+		return chatRoomService.findAllRoom();
+	}
+
+	public ChatRoom createChatRoom(String name) {
+        
+		ChatRoom chatRoom = chatRoomService.createChatRoom(name);
+        //채팅방의 사용자수 변경을 알린다 
+        messagingTemplate.convertAndSend("/sub/alarm", new AlarmMessageDTO(""));
+            
+        return chatRoom;
+    }
+
+	public ChatRoom findRoomById(String roomId) {
+		return chatRoomService.findRoomById(roomId);
+	}
+
+	public void userEnterChatRoomUser(String simpDestination, String sessionId, String sender) {
+		ChatRoom chatRoom = chatRoomService.userEnterChatRoomUser(simpDestination, sessionId, sender);
+		if (chatRoom != null) {
+	    //서버에서 클라이언트로 구독 메시지를 전달한다 
+			//채팅방의 입장한 모든 사용자에게 입장으로 알린다
+	    messagingTemplate.convertAndSend("/sub/chat/room/" + chatRoom.getRoomId()
+	    	, ChatMessageDTO.enterMessage(chatRoom, sender));
+
+	    //채팅방의 사용자수 변경을 알린다 
+	    messagingTemplate.convertAndSend("/sub/alarm", new AlarmMessageDTO(chatRoom.getRoomId()));
+		}
+	}
+
+	public void userLeaveChatRoomUser(String sessionId) {
+		ChatRoom chatRoom = chatRoomService.findRoomSessionId(sessionId);
+		if (chatRoom != null) {
+			String sender = chatRoom.getSender(sessionId);
+			System.out.println("userLeaveChatRoomUser() sender : " + sender);
+			chatRoomService.userLeaveChatRoomUser(sessionId);
+	    //서버에서 클라이언트로 구독 메시지를 전달한다 
+			//채팅방의 입장한 모든 사용자에게 퇴장으로 알린다
+	    messagingTemplate.convertAndSend("/sub/chat/room/" + chatRoom.getRoomId()
+	    	, ChatMessageDTO.leaveMessage(chatRoom, sender));
+		}
+    //채팅방의 사용자수 변경을 알린다 
+    messagingTemplate.convertAndSend("/sub/alarm", new AlarmMessageDTO(""));
+	}
 }
